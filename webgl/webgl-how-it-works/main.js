216280f36f950de53d9f9c348e42d32fef07f4fd
alvin.wang <alvin.wang@wayz.ai>
2022-10-17 15:36:05 +0800

    feat: add webgl-how-it-works

---
+++ webgl/webgl-how-it-works/main.js
@@ -0,0 +1,176 @@
+/* global webglLessonsUI m3 */
+
+import * as webglUtils from '../utils/webgl-utils'
+
+const vs = `#version 300 es
+in vec2 a_position;
+
+uniform mat3 u_matrix;
+
+out vec4 v_color;
+
+void main() {
+// Multiply the position by the matrix.
+gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
+
+// Convert from clipspace to colorspace.
+// Clipspace goes -1.0 to +1.0
+// Colorspace goes from 0.0 to 1.0
+v_color = gl_Position * 0.5 + 0.5;
+}
+`
+
+const fs = `#version 300 es
+
+precision highp float;
+
+in vec4 v_color;
+
+out vec4 outColor;
+
+void main() {
+outColor = v_color;
+}
+`
+
+function main() {
+  // Get A WebGL context
+  /** @type {HTMLCanvasElement} */
+  const canvas = document.querySelector('#canvas')
+  const gl = canvas.getContext('webgl2')
+  if (!gl) {
+    return
+  }
+
+  // setup GLSL program
+  const program = webglUtils.createProgramFromSources(gl, [vs, fs])
+
+  // look up where the vertex data needs to go.
+  const positionLocation = gl.getAttribLocation(program, 'a_position')
+
+  // lookup uniforms
+  const matrixLocation = gl.getUniformLocation(program, 'u_matrix')
+
+  // Create set of attributes
+  const vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
+
+  // Create a buffer.
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+
+  // Set Geometry.
+  setGeometry(gl)
+
+  // tell the position attribute how to pull data out of the current ARRAY_BUFFER
+  gl.enableVertexAttribArray(positionLocation)
+  const size = 2
+  const type = gl.FLOAT
+  const normalize = false
+  const stride = 0
+  const offset = 0
+  gl.vertexAttribPointer(
+    positionLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  )
+
+  const translation = [200, 150]
+  let angleInRadians = 0
+  const scale = [1, 1]
+
+  drawScene()
+
+  // Setup a ui.
+  webglLessonsUI.setupSlider('#x', {
+    value: translation[0],
+    slide: updatePosition(0),
+    max: gl.canvas.width
+  })
+  webglLessonsUI.setupSlider('#y', {
+    value: translation[1],
+    slide: updatePosition(1),
+    max: gl.canvas.height
+  })
+  webglLessonsUI.setupSlider('#angle', { slide: updateAngle, max: 360 })
+  webglLessonsUI.setupSlider('#scaleX', {
+    value: scale[0],
+    slide: updateScale(0),
+    min: -5,
+    max: 5,
+    step: 0.01,
+    precision: 2
+  })
+  webglLessonsUI.setupSlider('#scaleY', {
+    value: scale[1],
+    slide: updateScale(1),
+    min: -5,
+    max: 5,
+    step: 0.01,
+    precision: 2
+  })
+
+  function updatePosition(index) {
+    return function (event, ui) {
+      translation[index] = ui.value
+      drawScene()
+    }
+  }
+
+  function updateAngle(event, ui) {
+    const angleInDegrees = 360 - ui.value
+    angleInRadians = (angleInDegrees * Math.PI) / 180
+    drawScene()
+  }
+
+  function updateScale(index) {
+    return function (event, ui) {
+      scale[index] = ui.value
+      drawScene()
+    }
+  }
+
+  // Draw the scene.
+  function drawScene() {
+    webglUtils.resizeCanvasToDisplaySize(gl.canvas)
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+    // Clear the canvas
+    gl.clearColor(0, 0, 0, 0)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+    // Compute the matrix
+    let matrix = m3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight)
+    matrix = m3.translate(matrix, translation[0], translation[1])
+    matrix = m3.rotate(matrix, angleInRadians)
+    matrix = m3.scale(matrix, scale[0], scale[1])
+    // Tell it to use our program (pair of shaders)
+    gl.useProgram(program)
+
+    // Bind the attribute/buffer set we want.
+    gl.bindVertexArray(vao)
+
+    // Set the matrix.
+    gl.uniformMatrix3fv(matrixLocation, false, matrix)
+
+    // Draw the geometry.
+    const offset = 0
+    const count = 3
+    gl.drawArrays(gl.TRIANGLES, offset, count)
+  }
+}
+
+// Fill the buffer with the values that define a triangle.
+function setGeometry(gl) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array([0, -100, 150, 125, -175, 100]),
+    gl.STATIC_DRAW
+  )
+}
+
+main()
